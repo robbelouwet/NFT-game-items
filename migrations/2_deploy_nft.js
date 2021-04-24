27dbcd9ac4c5782c409b8cd1cde80845a20c4553
Robbe Louwet <robbe.louwet@the47.be>
2021-04-24 18:10:48 +0200

    refactored some require's to events, more tests implemented, deployed to ropsten net, rpc's and events working successfully

--- migrations/2_deploy_nft.js
+++
@@ -1,6 +0,0 @@
-const Collectible = artifacts.require("./Collectible.sol");
-//const Distributor = artifacts.require("./Distributor.sol");
-
-module.exports = async function (deployer) {
-	await deployer.deploy(Collectible);
-};
