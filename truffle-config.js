0c953246a6bed009c65840bbe4e73258fc3a8971
Robbe Louwet <robbe.louwet@the47.be>
2021-04-28 18:29:17 +0200

    implemented bundle.js, contract source code can now be successfully verified by pasting the Bundle.sol source code on etherscan

--- truffle-config.js
+++ truffle-config.js
@@ -1,45 +1,46 @@
-require("dotenv").config();
-const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider')
 
 module.exports = {
-	contracts_directory: "./contracts/bundle/",
-	networks: {
-		ropsten: {
-			provider: (_) =>
-				new HDWalletProvider(
-					process.env.ROPSTEN_MNEMONIC,
-					`https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`
-					//derivationPath: "m/44'/60'/0'/0/", // = default, maar toon voor learning purposes
-					//addressIndex: 0, // = default, pak het eerste address uit de wallet
-				),
-			network_id: 3,
-			gas: 5500000,
-			confirmations: 2,
-			timeoutBlocks: 200,
-			skipDryRun: true,
-		},
-		geth: {
-			host: "localhost",
-			port: 8545,
-			network_id: 3,
-			gas: 4700000,
-		},
-		development: {
-			// host is waar het development network runt
-			// dus localhost waar ganache runt op p 7545
-			host: "127.0.0.1",
-			port: 7545,
-			network_id: "*", // Match any network id
-			websockets: true
-		},
-	},
-	compilers: {
-		solc: {
-			version: "0.7.0",
-			optimizer: {
-				enabled: true,
-				runs: 200,
-			},
-		},
-	},
-};
+  contracts_directory: './contracts/bundle/',
+  networks: {
+    ropsten: {
+      provider: (_) =>
+        new HDWalletProvider({
+          mnemonic: process.env.ROPSTEN_MNEMONIC,
+          providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`,
+          //derivationPath: "m/44'/60'/0'/0/", // = default, maar toon voor learning purposes
+          //addressIndex: 0, // = default, pak het eerste address uit de wallet
+          chainId: 3,
+        }),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    geth: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 3,
+      gas: 4700000,
+    },
+    development: {
+      // host is waar het development network runt
+      // dus localhost waar ganache runt op p 7545
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*', // Match any network id
+      websockets: true,
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.7.0',
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+}
